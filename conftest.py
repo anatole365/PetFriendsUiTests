9a0d7126ace58b687b80a75e018a1217078240c9
av-gerasimov <124795814+av-gerasimov@users.noreply.github.com>
2023-04-05 12:11:17 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -7,7 +7,6 @@ def testing():
     pytest.driver.implicitly_wait(10)
     # Переход на страницу авторизации
     pytest.driver.get('http://petfriends.skillfactory.ru/login')
-    pytest.driver.implicitly_wait(10)
 
     yield
 
